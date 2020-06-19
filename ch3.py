94ff937e7f4bab50c5cc4c944559e324dc9e8d40
Ivypatel <65421640+Ivypatel@users.noreply.github.com>
2020-06-19 16:16:08 -0500

    Add files via upload

---
+++ ch3.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+thistuple = ["diary","book","pen","pencil"]
+print(thistuple)
+
+
+# In[9]:
+
+
+print(thistuple[0:2])
+
+
+# In[16]:
+
+
+thisdict = {"brand":"vitamin","year":2020,"color":"green"}
+print(thisdict)
+
+
+# In[2]:
+
+
+list1 = [5, 20, 15, 20, 25, 50, 20]
+
+def removeValue(sampleList, val):
+   return [value for value in sampleList if value != val]
+resList = removeValue(list1, 20)
+print(resList)
+
+
+# In[14]:
+
+
+A = [1,2,3,4,8,10,12,13,15,17]
+for i in A:
+    if i%2 == 0:
+        Evenlist.append(i)
+    else :
+        oddlist.append(i)
+print "Even numbers", EvenList
+print "odd numbers", OddList
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
